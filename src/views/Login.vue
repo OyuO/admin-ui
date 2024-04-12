5be64b6efb66343767a884f06bb17459d1793936
Pike <pike123456@qq.com>
2024-04-12 18:23:56 +0800

    Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -15,9 +15,17 @@ export default {
   <div class="login_container">
     <el-form>
       <h3 class="title">若依后台管理系统</h3>
-      <el-input v-model="username" placeholder="username"></el-input>
-      <el-input v-model="password" placeholder="password"></el-input>
-      <el-input style="width: 63%" v-model="code" placeholder="验证码"></el-input>
+      <el-form-item prop="username">
+        <el-input v-model="username" placeholder="username" prefix-icon="el-icon-user-solid"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input type="password" v-model="password" placeholder="password" prefix-icon="el-icon-lock"></el-input>
+      </el-form-item>
+      <el-form-item id="valid_code_form">
+        <el-input class="test" v-model="code" placeholder="验证码" prefix-icon="el-icon-circle-check"></el-input>
+        <img src="@/assets/images/validcode.gif"/>
+      </el-form-item>
+      <el-button type="primary" icon="el-icon-search">登录</el-button>
     </el-form>
   </div>
 </template>
@@ -45,6 +53,15 @@ export default {
   .el-input {
     margin-bottom: 10px;
   }
+}
+
+#valid_code_form {
+  display: flex;
+  align-items: center;
+}
+
+.test {
+  width: 49.9%;
 }
 
 </style>
