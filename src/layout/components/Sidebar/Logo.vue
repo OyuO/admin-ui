1e62844693f2ea814b0097ad978ce0edb758dd03
zhangyu <pike123456@qq.com>
2024-06-18 13:35:11 +0800

    feat(sidebar): 侧边栏logo样式

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -1,39 +1,77 @@
 <script>
 import logoImg from "@/assets/logo/logo.png"
+import variables from "@/assets/styles/variables.scss"
 
 export default {
+  name: "SidebarLogo",
   props: {
     collapse: {
       type: Boolean,
       required: true
     }
   },
+  computed: {
+    variables() {
+      return variables
+    },
+    sideTheme() {
+      return this.$store.state.settings.sideTheme
+    }
+  },
   data() {
     return {
+      title: process.env.VUE_APP_TITLE,
       logo: logoImg
     }
   }
 }
 </script>
 <template>
-  <div>
-    <router-link to="/index" v-if="collapse">
+  <div class="sidebar-logo-container" :class="{'collapse':collapse}"
+       :style="{backgroundColor:sideTheme === 'theme-dark' ? variables.menuBackground : variables.menuLightBackground}">
+    <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/index">
       <img :src="logo"/>
     </router-link>
-    <router-link class="logo-title-link" to="/index" v-else>
-      <img class="logo-img" :src="logo"/>
-      <span>若依管理系统</span>
+    <router-link key="expand" class="sidebar-logo-link" to="/index" v-else>
+      <img class="sidebar-logo" :src="logo"/>
+      <h1 class="sidebar-title"
+          :style="{ color: sideTheme === 'theme-dark' ? variables.logoTitleColor : variables.logoLightTitleColor }">
+        {{ title }}</h1>
     </router-link>
   </div>
 </template>
 
 <style scoped lang="scss">
-.logo-title-link {
-  display: inline-flex;
-  align-items: center
-}
+.sidebar-container {
+  position: relative;
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  background: #2b2f3a;
+  text-align: center;
+  overflow: hidden;
 
-.logo-img {
-  vertical-align: middle;
+  & .sidebar-logo-link {
+    height: 100%;
+    width: 100%;
+
+    & .sidebar-logo {
+      width: 32px;
+      height: 32px;
+      vertical-align: middle;
+      margin-right: 12px;
+    }
+
+    & .sidebar-title {
+      display: inline-block;
+      margin: 0;
+      color: #fff;
+      font-weight: 600;
+      line-height: 50px;
+      font-size: 14px;
+      font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
+      vertical-align: middle;
+    }
+  }
 }
 </style>
--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,7 +1,8 @@
 <script>
-import Logo from "./Logo"
 import {mapGetters, mapState} from "vuex"
+import Logo from "./Logo"
 import SidebarItem from "./SidebarItem.vue"
+import variables from "@/assets/styles/variables.scss"
 
 export default {
   components: {SidebarItem, Logo},
@@ -19,6 +20,9 @@ export default {
     showLogo() {
       return this.$store.state.settings.sidebarLogo
     },
+    variables() {
+      return variables
+    },
     isCollapse() {
       return !this.sidebar.opened
     }
@@ -31,10 +35,14 @@ export default {
 </script>
 
 <template>
-  <div>
+  <div :class="{'has-logo':showLogo}"
+       :style="{backgroundColor:settings.sideTheme === 'theme-dark' ? variables.menuBackground : variables.menuLightBackgroound}">
     <logo v-if="showLogo" :collapse="isCollapse"/>
-    <el-scrollbar :class="settings.sideTheme">
-      <el-menu :default-active="activeMenu" :collapse="isCollapse" :unique-opened="true" :collapse-transition="false"
+    <el-scrollbar :class="settings.sideTheme" wrap-class="scrollbar-wrapper">
+      <el-menu :default-active="activeMenu" :collapse="isCollapse"
+               :background-color="settings.sideTheme === 'theme-dark' ? variables.menuBackground : variables.menuLightBackground"
+               :text-color="settings.sideTheme === 'theme-dark' ? variables.menuColor : variables.menuLightColor"
+               :unique-opened="true" :collapse-transition="false"
                mode="vertical">
         <sidebar-item v-for="(route,index) in sidebarRouters" :key="route.path + index" :item="route"
                       :base-path="route.path"></sidebar-item>
